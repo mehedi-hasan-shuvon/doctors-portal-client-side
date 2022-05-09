ac2007a5d4fc1b5e03e9d62225c6265b6c2a376d
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-05-09 23:44:31 +0600

    added dynamic grid section

--- src/Pages/Home/InfoCard.js
+++ src/Pages/Home/InfoCard.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const InfoCard = ({ img }) => {
+const InfoCard = ({ img, cardTitle, bgClass }) => {
     return (
-        <div class="card lg:card-side bg-base-100 shadow-xl bg-accent">
+        <div class={`card lg:card-side bg-base-100 shadow-xl ${bgClass}`}>
             <figure className='pl-5'>
                 <img src={img} alt="Album" />
             </figure>
             <div class="card-body text-white">
-                <h2 class="card-title">New album is released!</h2>
+                <h2 class="card-title">{cardTitle}</h2>
                 <p>Click the button to listen on Spotiwhy app.</p>
             </div>
         </div>
