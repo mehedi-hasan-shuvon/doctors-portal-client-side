e8ee310727098962b004bddf8d5a82450206341a
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-05-11 22:17:07 +0600

    added contact us form

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from './Banner';
+import Contact from './Contact';
 import DentalCare from './DentalCare';
 import Info from './Info';
 import MakeAppointment from './MakeAppointment';
@@ -15,6 +16,7 @@ const Home = () => {
             <DentalCare></DentalCare>
             <MakeAppointment></MakeAppointment>
             <Testimonials></Testimonials>
+            <Contact></Contact>
         </div>
     );
 };
