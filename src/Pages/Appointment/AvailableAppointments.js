0b02a4cda1bff63d9163bbfa6b76ee068fcfefe4
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-05-15 02:34:56 +0600

    implemented google authentication

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -17,7 +17,7 @@ const AvailableAppointments = ({ date }) => {
 
     return (
         <div>
-            <h4 className='text-xl text-center text-secondary'>Available Appointments on  {format(date, 'PP')}</h4>
+            <h4 className='text-xl text-center text-secondary my-12'>Available Appointments on  {format(date, 'PP')}</h4>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
                 {
                     services.map((service) =>
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+
+    if (user) {
+        console.log(user);
+    }
+
+
     return (
-        <div>
-            <h2>This is login page</h2>
+        <div className='flex justify-center items-center h-screen'>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <div className="card-body">
+                    <h2 className="text-center text-2xl font-bold">Login</h2>
+
+                    <div className="divider">OR</div>
+                    <button
+                        onClick={() => signInWithGoogle()}
+                        className="btn btn-outline"
+                    >Continue with Google</button>
+
+                </div>
+            </div>
         </div>
     );
 };
