bdec516a129c92796e257a604b79fd79da3fe4af
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-05-17 02:07:56 +0600

    added signout functionality in the nav bar

--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { useCreateUserWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword, useSignInWithGoogle, useUpdateProfile } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import { useForm } from "react-hook-form";
 import Loading from '../Shared/Loading';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
 
     const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
     const { register, formState: { errors }, handleSubmit } = useForm();
+
+
+    // update user profile 
+    const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+
+
+    //for navigation after signup or updating
+    const navigate = useNavigate();
+
 
     let signInErrorMessage;
 
@@ -27,17 +36,19 @@ const SignUp = () => {
     }
 
 
-    const onSubmit = data => {
-        console.log(data);
-        createUserWithEmailAndPassword(data.email, data.password);
+    const onSubmit = async data => {
+        await createUserWithEmailAndPassword(data.email, data.password);
+        await updateProfile({ displayName: data.name });
+        console.log("update done");
+        navigate('/appointment');
     }
 
-    if (loading || gLoading) {
+    if (loading || gLoading || updating) {
         return <Loading></Loading>
     }
 
-    if (error || gError) {
-        signInErrorMessage = <p className='text-red-500'><small>{error?.message || gError?.message}</small></p>
+    if (error || gError || updateError) {
+        signInErrorMessage = <p className='text-red-500'><small>{error?.message || gError?.message || updateError?.message}</small></p>
     }
 
 
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,14 +1,29 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 const Navbar = () => {
+
+    const [user, loading, error] = useAuthState(auth);
+
+    const logout = () => {
+        signOut(auth);
+    };
+
     const menuItems = <>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/appointment">Appointment</Link></li>
         <li><Link to="/review">Review</Link></li>
         <li><Link to="/contact">Contact</Link></li>
         <li><Link to="/about">About</Link></li>
-        <li><Link to="/Login">Login</Link></li>
+        <li>{
+            user ?
+                <button onClick={logout} class="btn btn-ghost">Sign Out</button>
+                :
+                <Link to="/Login">Login</Link>
+        }</li>
     </>
     return (
         <div className="navbar bg-base-100">
