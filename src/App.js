3acd7a5c2aba4a358675fa7e0ac4a69bf0ce0fc8
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-05-23 01:09:26 +0600

    added manage doctors page to manage the doctors by admin

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ import MyHistroy from './Pages/Dashboard/MyHistroy';
 import Users from './Pages/Dashboard/Users';
 import RequireAdmin from './Pages/Login/RequireAdmin';
 import AddDoctor from './Pages/Dashboard/AddDoctor';
+import ManageDoctors from './Pages/Dashboard/ManageDoctors';
 
 
 function App() {
@@ -43,6 +44,7 @@ function App() {
           <Route path='history' element={<MyHistroy></MyHistroy>}></Route>
           <Route path='users' element={<RequireAdmin><Users></Users></RequireAdmin>}></Route>
           <Route path='addDoctor' element={<RequireAdmin><AddDoctor></AddDoctor></RequireAdmin>}></Route>
+          <Route path='manageDoctor' element={<RequireAdmin><ManageDoctors></ManageDoctors></RequireAdmin>}></Route>
         </Route>
         <Route path='login' element={<Login></Login>}></Route>
         <Route path='signup' element={<SignUp></SignUp>}></Route>
--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -29,6 +29,7 @@ const Dashboard = () => {
                     {admin && <>
                         <li><Link to="/dashboard/users">All Users</Link></li>
                         <li><Link to="/dashboard/addDoctor">Add a Doctor</Link></li>
+                        <li><Link to="/dashboard/manageDoctor">Manage Doctors</Link></li>
                     </>}
                 </ul>
 
